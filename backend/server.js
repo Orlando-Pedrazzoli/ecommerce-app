dd30c23db6436f8e17d36b743ffe1c95dbcbf127
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-05-09 22:12:40 +0100

    back end changes

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,15 @@ connectDB();
 connectCloudinary();
 
 // middlewares
-app.use(express.json());
-app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(
+  cors({
+    origin: 'https://www.elitesurfingpedidos.com.br',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 
 // api endpoints
 app.use('/api/user', userRouter);
